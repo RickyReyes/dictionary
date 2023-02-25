950d947f2723dbec80cfcc6a1aab44bd1f8a70e0
RickyReyes <77644172+RickyReyes@users.noreply.github.com>
2023-02-25 03:33:45 -0800

    context working...word not showing now!

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import Header from "./layouts/Header/Header";
 import Input from "./layouts/Input/Input";
 import Word from "./Word";
 import Source from "./Source";
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 import Meaning from "./Meaning";
+import NotFound from "./NotFound";
+import { ThemeContext, IThemeContext } from "./themeContext";
 
 export interface IWord {
   license: {
@@ -42,10 +44,36 @@ export interface IMeaning {
 }
 
 function App() {
+  const [font, setFont] = useState<string>("sans-serif");
+  const [searching, setSearching] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [notFound, setNotFound] = useState<boolean>(false);
   const [showFontModal, setShowFontModal] = useState<boolean>(false);
-  const [font, setFont] = useState<string>("Mono");
+
   const [wordObj, setWordObj] = useState<null | IWord>(null);
 
+  useEffect(() => {
+    if (searching && inputValue.length > 0 && wordObj?.word !== inputValue) {
+      fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
+        .then((res) => {
+          if (!res.ok) {
+            setNotFound(true);
+            throw new Error("No such word found.");
+          } else {
+            setNotFound(false);
+            return res.json();
+          }
+        })
+        .then((data) => setWordObj(data[0]))
+        .catch((err) => {
+          setNotFound(true);
+          return err;
+        });
+      setSearching(false);
+    }
+  }, [searching]);
+
+  const { theme } = useContext<IThemeContext>(ThemeContext);
   return (
     <main className={"App " + font}>
       <Header
@@ -54,13 +82,25 @@ function App() {
         showFontModal={showFontModal}
         setShowFontModal={setShowFontModal}
       />
-      <Input setWordObj={setWordObj} />
-      {wordObj && <Word wordObj={wordObj} />}
-      {wordObj?.meanings.map((meaning) => (
-        <Meaning meaning={meaning} />
-      ))}
-      <hr></hr>
-      <Source wordObj={wordObj} />
+      <Input
+        searching={searching}
+        setSearching={setSearching}
+        inputValue={inputValue}
+        setInputValue={setInputValue}
+        setWordObj={setWordObj}
+      />
+      {searching && !wordObj && <p className="searching">Searching...</p>}
+      {notFound && <NotFound />}
+      {wordObj && (
+        <>
+          <Word wordObj={wordObj} />
+          {wordObj?.meanings.map((meaning) => (
+            <Meaning meaning={meaning} />
+          ))}
+          <hr></hr>
+          <Source wordObj={wordObj} />
+        </>
+      )}
     </main>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/style.css";
+import { ThemeContextProvider } from "./themeContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeContextProvider>
+      <App />
+    </ThemeContextProvider>
   </React.StrictMode>
 );
